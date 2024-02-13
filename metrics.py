03b7799d35f69ed7e62a9d68b5411f8f8db4d1a3
John Quinn <jquinn@cit.ac.ug>
2024-02-13 00:01:40 +0000

    Handle missing metrics

--- metrics.py
+++ metrics.py
@@ -10,6 +10,11 @@ def multilingual_eval(eval_preds,
                       tokenizer,
                       log_first_N_predictions):
     '''Compute metric scores for each source and target language combination.'''
+    def _round_if_float(f, p):
+      if isinstance(f, float):
+        return round(f, p)
+      else:
+        return f
     
     predictions, labels = eval_preds
     decoded_predictions = tokenizer.batch_decode(
@@ -28,7 +33,7 @@ def multilingual_eval(eval_preds,
 
     subsets = {}
     for i in range(len(predictions)):
-        language_combination = source_language[i] + '2' + target_language[i]
+        language_combination = source_language[i] + '_' + target_language[i]
         if language_combination not in subsets:
             subsets[language_combination] = {'predictions': [], 'labels': []}
         subsets[language_combination]['predictions'].append(
@@ -50,12 +55,17 @@ def multilingual_eval(eval_preds,
                 raise ValueError('Only BLEU and WER metrics currently '
                                  'supported.')
             result[f'{metric_name}_{subset}'] = r 
-        
-    result[f'{metric_name}_mean'] = np.mean(
-        [result[f'{metric_name}_{subset}'] for subset in list(subsets.keys())]
-    )
+            
+            
+        subset_values = [result[f'{metric_name}_{subset}']
+                         for subset in list(subsets.keys())]  
+        try:
+            result[f'{metric_name}_mean'] = np.mean(subset_values)
+        except TypeError:
+            result[f'{metric_name}_mean'] = np.nan
 
-    result = {k: round(v, 3) for k, v in result.items()}
+    result = {k: _round_if_float(v, 3) for k, v in result.items()}
+    print('result', result)
     return result
 
 def multilingual_eval_fn(eval_dataset,
